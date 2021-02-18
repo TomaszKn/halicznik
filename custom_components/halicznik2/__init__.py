07964fe6f297f7424d05fa8b7778e469005325cb
TomaszKn <tomasz.knura@gmail.com>
2021-02-18 07:35:24 +0100

    poprawka kodu

--- custom_components/halicznik2/__init__.py
+++ custom_components/halicznik2/__init__.py
@@ -56,10 +56,11 @@ def _ustawienia(hass, config):
 
 async def async_setup(hass: HomeAssistant, config: Config) -> bool:
     """AMS hub YAML setup."""
+    _ustawienia(hass, config[DOMAIN])
     if config.get(DOMAIN) is None:
         _LOGGER.info("No YAML config available, using config_entries")
         return True
-    _ustawienia(hass, config[DOMAIN])
+    #_ustawienia(hass, config[DOMAIN])
     if not hass.config_entries.async_entries(DOMAIN):
         hass.async_create_task(
             hass.config_entries.flow.async_init(
