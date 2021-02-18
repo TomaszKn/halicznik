c20d22553ebb6b991b0e114ccdd16e078e6a7c9a
TomaszKn <tomasz.knura@gmail.com>
2021-02-18 20:28:46 +0100

    dodano testowy kod ec3

--- custom_components/halicznik2/parsers/EC3.py
+++ custom_components/halicznik2/parsers/EC3.py
@@ -15,7 +15,7 @@ def parse_data(stored, data):
     han_data = {}
     pkt = data
 
-
+    """
     han_data["obis_r_e_n"] = field_type(".", fields=pkt[240:246])
     han_data["reactive_energy_n"] = byte_decode(fields=pkt[247:251]) / 100
     sensor_data["ams_reactive_energy_export"] = {
@@ -31,6 +31,19 @@ def parse_data(stored, data):
             "icon": "mdi:gauge",
         },
     }
-
+    """
+    sensor_data["pomiar"] = {
+        "state": "12",
+        "attributes": {
+            "timestamp": "2021-02-12",
+            "meter_timestamp": "2021-02-13",
+            "meter_manufacturer": "Tit",
+            "meter_type": "Energy",
+            "meter_serial": 12345,
+            "obis_code": "0.12.13",
+            "unit_of_measurement": "kVArh",
+            "icon": "mdi:gauge",
+        },
+    }
     stored.update(sensor_data)
     return stored, han_data
