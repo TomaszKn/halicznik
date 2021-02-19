9a95c80cac413b2d439b1722bc1b7469bb9b78e2
TomaszKn <tomasz.knura@gmail.com>
2021-02-19 19:24:25 +0100

    dodano flow

---
+++ custom_components/halicznik2/config_flow.pl.py
@@ -0,0 +1,85 @@
+"""Adds config flow for hass-AMS."""
+import logging
+import os
+
+import serial.tools.list_ports as devices
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import (
+    CONF_METER_MANUFACTURER,
+    #CONF_PARITY,
+    CONF_SERIAL_PORT,
+    DEFAULT_METER_MANUFACTURER,
+    #DEFAULT_PARITY,
+    DOMAIN,
+    MANUFACTURER_OPTIONS,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class AmsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for AMS."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
+    def __init__(self):
+        """Initialize."""
+        self._errors = {}
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        portdata = await self.hass.async_add_executor_job(devices.comports)
+        ports = [(comport.device + ": " + comport.description) for comport in portdata]
+
+        if user_input is not None:
+            user_selection = user_input[CONF_SERIAL_PORT]
+            port = portdata[ports.index(user_selection)]
+            serial_by_id = await self.hass.async_add_executor_job(
+                get_serial_by_id, port.device
+            )
+            user_input[CONF_SERIAL_PORT] = serial_by_id
+            return self.async_create_entry(title="Norwegian AMS", data=user_input)
+        _LOGGER.debug(ports)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_SERIAL_PORT, default=None): vol.In(ports),
+                    vol.Required(
+                        CONF_METER_MANUFACTURER, default=DEFAULT_METER_MANUFACTURER
+                    ): vol.In(MANUFACTURER_OPTIONS),
+                    #vol.Optional(CONF_PARITY, default=DEFAULT_PARITY): vol.All(str),
+                }
+            ),
+            description_placeholders={
+                CONF_SERIAL_PORT: ports,
+                CONF_METER_MANUFACTURER: MANUFACTURER_OPTIONS,
+            },
+            errors=self._errors,
+        )
+
+    async def async_step_import(self, import_config):
+        """Import a config flow from configuration."""
+        if self._async_current_entries():
+            _LOGGER.warning("Only one configuration of hass-AMS is allowed.")
+            return self.async_abort(reason="singel_instance_allowed")
+
+        return self.async_create_entry(title="configuration.yaml", data=import_config)
+
+
+def get_serial_by_id(dev_path):
+    """Return a /dev/serial/by-id match for given device if available."""
+    by_id = "/dev/serial/by-id"
+    if not os.path.isdir(by_id):
+        return dev_path
+
+    for path in (entry.path for entry in os.scandir(by_id) if entry.is_symlink()):
+        if os.path.realpath(path) == dev_path:
+            return path
+    return dev_path
